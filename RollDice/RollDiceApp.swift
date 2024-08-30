7ecdc6ed37b0f17b83799c68afdd0c80b6bd7e41
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-30 06:03:24 +0200

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Adam Elfsborg on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
